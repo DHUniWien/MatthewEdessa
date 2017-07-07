da13656771f3fbed54a2283b978f6311155e1782
Tara L Andrews <tla@mit.edu>
2017-07-07 10:39:42 +0200

    extract a single page too

--- tools/tpen_extract.py
+++ tools/tpen_extract.py
@@ -5,9 +5,15 @@ msdata = None
 with open(sys.argv[1], encoding='utf-8') as jfile:
     msdata = json.load(jfile)
 
+pgtag = None
+if len(sys.argv) > 2:
+    pgtag = sys.argv[2]
+
 pages = msdata['sequences'][0]['canvases']
 lines = []
 for page in pages:
+    if pgtag is not None and page['label'].find(pgtag) < 0:
+        continue
     pglines = [x['resource']['cnt:chars'] for x in page['otherContent'][0]['resources']]
     lines.append(''.join(pglines))
 
