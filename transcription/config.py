c6153a5312fc286f1181e66b66e9b24ee4ec1e82
Tara L Andrews <tla@mit.edu>
2018-09-13 22:38:23 +0200

    get rid of standalone 'added' hyphens

--- transcription/config.py
+++ transcription/config.py
@@ -80,6 +80,8 @@ def transcription_filter(line):
     """A list of custom corrections to easily-fixed transcription errors"""
     line = re.sub(r'(?<=[^</"])un(clear|known)', '<gap/>', line)
     line = re.sub(r'<del\s+type', '<del rend', line)
+    # fix hyphen misinterpretation of Tatevik V.
+    line = re.sub(r'\s*<add place="below">֊</add>', '֊', line)
     return line.replace(
         '_', '֊').replace(  # fix erroneous underscore use by Razmik
         '“', '"').replace(  # fix curly quote pasting by Anahit
